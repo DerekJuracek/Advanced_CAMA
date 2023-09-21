e4f4e3dc1a1c5fdef747308b41b8f22ea9679cdf
Derek Juracek <dajuracek@gmail.com>
2023-09-21 19:47:10 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,74 @@
+require([
+  "esri/WebMap",
+  "esri/views/MapView",
+  "esri/layers/FeatureLayer",
+  "esri/widgets/Features",
+  "esri/core/reactiveUtils",
+], function (WebMap, MapView, FeatureLayer, Features, reactiveUtils) {
+  const webmap = new WebMap({
+    portalItem: {
+      id: "6448b08504de4244973a28305b18271f",
+    },
+  });
+
+  var view = new MapView({
+    container: "viewDiv",
+    map: webmap,
+    zoom: 12,
+    popupEnabled: false,
+  });
+
+  const featureLayer = new FeatureLayer({
+    url: "https://services1.arcgis.com/j6iFLXhyiD3XTMyD/arcgis/rest/services/CT_Washington_Adv_Viewer_Parcels/FeatureServer/1",
+  });
+  webmap.add(featureLayer);
+
+  const featuresWidget = new Features({
+    container: "feature-wid",
+    view: view,
+  });
+
+  reactiveUtils.on(
+    () => view,
+    "click",
+    (event) => {
+      featuresWidget.open({
+        location: event.mapPoint,
+        fetchFeatures: true,
+      });
+    }
+  );
+
+  $(document).ready(function () {
+    $("#dropdownMenuButton").on("click", function () {
+      $("#dropdown").toggleClass("expanded");
+      if ($("#dropdown").hasClass("expanded")) {
+        $("#up-arrow").show();
+        $("#down-arrow").hide(); // Hide left arrow
+        // Show right arrow
+      } else {
+        $("#down-arrow").show(); // Show left arrow
+        $("#up-arrow").hide(); // Hide right arrow
+      }
+    });
+
+    $(document).ready(function () {
+      $("#side-Exp").on("click", function () {
+        $("#sidebar").toggleClass("collapsed");
+
+        // Check if the sidebar has the class 'collapsed' and adjust the 'right' property of #small-div accordingly
+        // Toggle the 'right' CSS property for #small-div
+        if ($("#sidebar").hasClass("collapsed")) {
+          $("#small-div").css("right", "0px");
+          $("#right-arrow").hide();
+          $("#left-arrow").show(); // Hide left arrow
+          // Show right arrow
+        } else {
+          $("#small-div").css("right", "250px");
+          $("#left-arrow").hide(); // Show left arrow
+          $("#right-arrow").show(); // Hide right arrow
+        }
+      });
+    });
+  });
+});
